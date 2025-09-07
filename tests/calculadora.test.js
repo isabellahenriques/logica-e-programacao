da3b66147836e3b83fcc3ba170cbe2776ee93b19
Isabella Henriques Szrajbman <68662472+isabellahenriques@users.noreply.github.com>
2025-09-07 18:25:48 -0300

    Delete tests directory

--- tests/calculadora.test.js
+++
@@ -1,6 +0,0 @@
-// Chamando/importando a função que quero testar
-const {somarDoisNumeros} = require('../src/calculadora');
-
-const resultadoDaSoma = somarDoisNumeros(3, 7);
-
-console.log(resultadoDaSoma); // 10
